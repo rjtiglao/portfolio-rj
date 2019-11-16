e10128db8af5706b457d423ec444d4941b644bf8
Rj Tiglao <rjtiglao@gmail.com>
2019-11-15 20:02:43 -0800

    Basic Template Configuration

---
+++ models/example.js
@@ -0,0 +1,23 @@
+
+// Mongoose configuration
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//SCHEMA EXAMPLE:
+
+// Step 1
+// const bookSchema = new Schema({
+//   title: { type: String, required: true },
+//   author: { type: String, required: true },
+//   synopsis: String,
+//   date: { type: Date, default: Date.now }
+// });
+
+// Schemal Constructor EXAMPLE
+
+// Step 2
+// const Book = mongoose.model("Book", bookSchema);
+
+// Step 3
+// Module export example
+// module.exports = Book;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // This is an example of exporting the model
+    // Example: require("./example")
+  };
+  
